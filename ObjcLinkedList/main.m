5dbc1a81a94f11ed4046d971b45b07c4d03f9b03
Halo Lee <matrixlmsly@gmail.com>
2015-05-28 13:56:48 +1000

    Initial Commit

---
+++ ObjcLinkedList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjcLinkedList
+//
+//  Created by Halo Lee on 28/05/2015.
+//  Copyright (c) 2015 halolee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
