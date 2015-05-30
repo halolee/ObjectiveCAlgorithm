d927ab16b69a62c82bc443151d8fee551d84df85
Halo Lee <matrixlmsly@gmail.com>
2015-05-30 23:30:50 +1000

    Start working on stack

---
+++ ObjcLinkedList/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  ObjcLinkedList
+//
+//  Created by Hao Li on 30/05/2015.
+//  Copyright (c) 2015 halolee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+@end
---
+++ ObjcLinkedList/Stack.m
@@ -0,0 +1,13 @@
+//
+//  Stack.m
+//  ObjcLinkedList
+//
+//  Created by Hao Li on 30/05/2015.
+//  Copyright (c) 2015 halolee. All rights reserved.
+//
+
+#import "Stack.h"
+
+@implementation Stack
+
+@end
